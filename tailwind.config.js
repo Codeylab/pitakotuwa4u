1a3283087058a07fe63623832ff6fd0334633928
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2021-03-30 15:55:34 +0600

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        "text-blue": "#05A4DB",
+        "light-blue": "#00B1EF",
+        "light-blue2": "#C9F1FF",
         "light-gray": "#E9F6FB",
         dark: "#363636",
+        "border-light": "#E0E0E0",
+        paragraph: "#A8A8A8",
       },
       screens: {
         "2xl": "1440px",
