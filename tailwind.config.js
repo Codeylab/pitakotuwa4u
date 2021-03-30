10298a4ce37f796b5bfdf9424b94eaef3ec7becf
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2021-03-31 01:14:45 +0600

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        blue: "#3B4ACC",
         "text-blue": "#05A4DB",
         "light-blue": "#00B1EF",
         "light-blue2": "#C9F1FF",
         "light-gray": "#E9F6FB",
         dark: "#363636",
+        dark2: "#454545",
         "border-light": "#E0E0E0",
         paragraph: "#A8A8A8",
         pink: "#BD3E96",
         "light-pink": "#FFAAE5",
       },
-      backgroundSize: {
-        "50%": "50% 50%",
-      },
       screens: {
         "2xl": "1440px",
+      },
+      fontFamily: {
+        gloria: "'Gloria Hallelujah', cursive",
       },
     },
   },
